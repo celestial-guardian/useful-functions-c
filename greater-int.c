faa837c044a255e1a968374bec8ab52b78f0481c
Artem <artem.litov4enko@gmail.com>
2015-01-11 15:10:24 +0200

    Create greater-int.c

---
+++ greater-int.c
@@ -0,0 +1,13 @@
+/*
+ * Author: Me
+ * Date: Jan 2015
+ * About: function receives two integer arguments and returns greater of them,
+ * if arguments are equal than it returns first of them
+ */
+ 
+ int greater_int(int a, int b){
+	if(a >= b)
+		return a;
+	else
+		return b;
+ }
