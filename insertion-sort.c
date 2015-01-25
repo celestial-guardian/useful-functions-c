f974c1483dd86b38e8faf2a5553e7d42bc4383f8
Artem <artem.litov4enko@gmail.com>
2015-01-25 19:07:39 +0200

    Insertion sort for an array of interger with length N

---
+++ insertion-sort.c
@@ -0,0 +1,20 @@
+/* 
+ * Author: Me & Wikipedia
+ * Date: 25 Jan 2015
+ * About: Insertion sort of integer array with length N.
+ */
+
+void insertion_sort(int *array, unsigned N){
+	int i, current, prevKey;
+	
+	for(i = 1; i < N; ++i){
+		current = array[i];
+		prevKey = i - 1;
+		
+		while(prevKey >= 0 && array[prevKey] > current){
+			array[prevKey + 1] = array[prevKey];
+			array[prevKey] = current;
+			prevKey = prevKey - 1;
+		}
+	}
+}
