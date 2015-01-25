40012edbc546e8e1e4aa3bf2e9fca30fddf59c82
Artem <artem.litov4enko@gmail.com>
2015-01-25 14:49:34 +0200

    Create shell-sort.c

---
+++ shell-sort.c
@@ -0,0 +1,28 @@
+/* 
+ * Author: Me & Wikipedia
+ * Date: 25 Jan 2015
+ * About: Shell sort of integer array with length N.
+ * d(1) = N/2, d(i) = d(i-2)/2, d(k) = 1.
+ */
+ 
+void shell_sort(int *array, unsigned N){
+	unsigned i, j, k;
+	int temporary;
+	
+	for(k = N/2; k > 0; k /= 2){
+		for(i = k; i < N; i++){
+			temporary = array[i];
+			
+			for(j = i; j >= k; j -= k){
+				if(temporary < array[j - k]){
+					array[j] = array[j - k];
+				}
+				else{
+					break;
+				}
+			}
+			
+			array[j] = temporary;
+		}
+	}
+}
